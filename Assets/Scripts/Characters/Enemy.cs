6c1d2d6a2e59067fb65a1557c24728fecdd5635b
145566854+VFil130@users.noreply.github.com <145566854+VFil130@users.noreply.github.com>
2025-10-26 13:39:28 +0300

    damage feedback

--- Assets/Scripts/Characters/Enemy.cs
+++ Assets/Scripts/Characters/Enemy.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.TextCore.Text;
@@ -7,6 +8,7 @@ public class Enemy : MonoBehaviour
 {
     public EnemyScriptableObject enemyData;
 
+    [SerializeField] private Color damageColor; 
     [SerializeField] private float currentHealth;
     private float currentMoveSpeed;
     private float currentDamage;
@@ -35,6 +37,7 @@ public class Enemy : MonoBehaviour
         float totalDmg = damage - currentPhysArmour;
         if (totalDmg < 0) { totalDmg = 0; }
         currentHealth -= totalDmg;
+        TakeDamageLogick();
         //UpdateHealthBar();
     }
     public void TakeMageDamage(float damage)
@@ -42,7 +45,22 @@ public class Enemy : MonoBehaviour
         float totalDmg = damage - currentMageArmour;
         if (totalDmg < 0) { totalDmg = 0; }
         currentHealth -= totalDmg;
+        TakeDamageLogick();
         //UpdateHealthBar();
+    }
+    public void TakeDamageLogick()
+    {
+        StartCoroutine(DamageFeedBack());
+    }
+    private IEnumerator DamageFeedBack()
+    {
+        Vector3 prevSize;
+        prevSize = transform.localScale; 
+        transform.localScale *= 1.1f;
+        GetComponent<SpriteRenderer>().color = damageColor;
+        yield return new WaitForSeconds(0.2f);
+        transform.localScale = prevSize;
+        GetComponent<SpriteRenderer>().color = Color.white;
     }
     public void Die()
     {
