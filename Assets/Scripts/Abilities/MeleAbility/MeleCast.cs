15a070dd214eb8942b6058b654c832baed202329
145566854+VFil130@users.noreply.github.com <145566854+VFil130@users.noreply.github.com>
2026-03-29 21:19:15 +0300

    abilities2

--- Assets/Scripts/Abilities/MeleAbility/MeleCast.cs
+++ Assets/Scripts/Abilities/MeleAbility/MeleCast.cs
@@ -4,33 +4,43 @@ using UnityEngine;
 public class MeleCast : AbilityStats
 {
     [SerializeField] protected GameObject MelePrefab;
-    protected GameObject currentMelePrefab;
+    protected List<GameObject> currentMeleePrefabs = new List<GameObject>();
     void Update()
     {
-        if (currentMelePrefab != null)
+        for (int i = currentMeleePrefabs.Count - 1; i >= 0; i--)
         {
-            Melee melee = currentMelePrefab.GetComponent<Melee>();
-            if (melee.destroy)
+            if (currentMeleePrefabs[i] == null)
             {
-                Destroy(currentMelePrefab);
+                currentMeleePrefabs.RemoveAt(i);
+                continue;
+            }
+
+            Melee melee = currentMeleePrefabs[i].GetComponent<Melee>();
+            if (melee != null && melee.destroy)
+            {
+                Destroy(currentMeleePrefabs[i]);
+                currentMeleePrefabs.RemoveAt(i);
             }
         }
     }
     public void CreateMele()
     {
-        currentMelePrefab = Instantiate(MelePrefab, abilityCaster.transform.position, abilityCaster.transform.rotation);
-        currentMelePrefab.transform.SetParent(abilityCaster.transform);
-        Melee meleeScript = currentMelePrefab.GetComponent<Melee>();
+        GameObject meleeObj = Instantiate(MelePrefab, abilityCaster.transform.position, abilityCaster.transform.rotation);
+        meleeObj.transform.SetParent(abilityCaster.transform);
+        Melee meleeScript = meleeObj.GetComponent<Melee>();
         meleeScript.Initialize(this);
+        currentMeleePrefabs.Add(meleeObj);
     }
+
     public void CreateMeleInLocalPosition(float x, float y)
     {
         Vector3 offset = new Vector3(x, y, 0);
-        currentMelePrefab = Instantiate(MelePrefab, abilityCaster.transform.position, abilityCaster.transform.rotation);
-        currentMelePrefab.transform.SetParent(abilityCaster.transform);
-        currentMelePrefab.transform.localPosition = offset;
-        currentMelePrefab.transform.localRotation = Quaternion.identity;
-        Melee meleeScript = currentMelePrefab.GetComponent<Melee>();
+        GameObject meleeObj = Instantiate(MelePrefab, abilityCaster.transform.position, abilityCaster.transform.rotation);
+        meleeObj.transform.SetParent(abilityCaster.transform);
+        meleeObj.transform.localPosition = offset;
+        meleeObj.transform.localRotation = Quaternion.identity;
+        Melee meleeScript = meleeObj.GetComponent<Melee>();
         meleeScript.Initialize(this);
+        currentMeleePrefabs.Add(meleeObj);
     }
 }
